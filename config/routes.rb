d795c6d9be59a48fa672c5827ce20e0de1700a0f
Ross Young <ross.young12@gmail.com>
2016-02-24 10:54:19 -0600

    adds namespace in config and moves files into admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :brands
-  resources :products
-  resources :categories
+
+  namespace :admin do
+    resources :brands
+    resources :products
+    resources :categories
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
