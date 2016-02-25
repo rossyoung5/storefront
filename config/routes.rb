23071b1b895519b6afc0432619655ea67241d8c6
Ross Young <ross.young12@gmail.com>
2016-02-25 08:42:57 -0600

    adds navbar and bootstrap to tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/index' => 'page#index'
 
   namespace :admin do
     resources :brands
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'page#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
