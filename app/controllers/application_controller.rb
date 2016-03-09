583284be662d37dccef6ccec1f0a93a0cf02244c
Ross Young <ross.young12@gmail.com>
2016-03-09 09:57:09 -0600

    adds admin to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def require_admin_user
+    unless user_signed_in? && current_user.admin
+      redirect_to root_url, alert: "YOU...SHALL NOT...PASS (If you're an admin, sign in to access)"
+    end
+  end
+
 end
